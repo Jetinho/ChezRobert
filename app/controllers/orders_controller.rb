fef0257d10ff9468239ed133f945384ef64acac3
Julien Jet <jet.julien@gmail.com>
2016-12-03 01:05:45 -0200

    Alter production name, build basic journey from create product to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :set_production, only: [:new, :create]
   def index
   end
 
@@ -6,9 +7,19 @@ class OrdersController < ApplicationController
   end
 
   def new
+    @order = Order.new
   end
 
   def create
+    @order = @production.orders.build(order_params)
+    @order.status = "En attente de confirmation"
+    @order.user = current_user
+      binding.pry
+    if @order.save
+      redirect_to production_order_path(@production, @order)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,4 +27,15 @@ class OrdersController < ApplicationController
 
   def update
   end
+
+  private
+
+  def set_production
+    @production = Production.find(params[:production_id])
+  end
+
+  def order_params
+    params.require(:order).permit(:quantity)
+  end
+
 end
--- app/controllers/productions_controller.rb
+++ app/controllers/productions_controller.rb
@@ -1,14 +1,25 @@
 class ProductionsController < ApplicationController
+  before_action :set_production, only: [:show, :edit, :destroy]
   def index
+    @productions = Production.all
   end
 
   def show
   end
 
   def new
+    @production = Production.new
   end
 
   def create
+    @production = Production.new(production_params)
+    @production.user = current_user
+    @production.status = "Disponible à la vente"
+    if @production.save
+      redirect_to production_path(@production)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +29,15 @@ class ProductionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def production_params
+    params.require(:production).permit(:name, :quantity, :unit, :price, :price_unit)
+  end
+
+  def set_production
+    @production = Production.find(params[:id])
   end
 end
