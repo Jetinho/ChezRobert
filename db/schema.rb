6a3c1ce19449a7a886b4d6911b318e7fc5af8e05
Julien Jet <jet.julien@gmail.com>
2016-12-04 16:29:53 -0200

    Add 4 last prods to HP

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203013235) do
+ActiveRecord::Schema.define(version: 20161203134711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20161203013235) do
     t.boolean  "delivery"
     t.text     "delivery_conditions"
     t.integer  "delivery_range"
+    t.boolean  "producer"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
